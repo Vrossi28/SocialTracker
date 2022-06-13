67b911fdcd73bdb41aa9c11491b25d64c11612e8
Vrossi28 <vrossi28@outlook.com>
2022-06-13 15:01:21 +0400

    Implemeting TweetsController and methods

---
+++ Analytics/Controllers/Twitter/TweetsController.cs
@@ -0,0 +1,40 @@
+﻿using ExternalIntegration.Twitter.Interfaces.Tweets;
+using ExternalIntegration.Twitter.Models.Tweets;
+using ExternalIntegration.Twitter.Requests;
+using ExternalIntegration.Twitter.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Analytics.Controllers.Twitter
+{
+    public class TweetsController : Controller
+    {
+        [HttpGet]
+        [Route("Tweets/SingleTweet/{id}")]
+        public async Task<DefaultResponse<ITweetsAllData>> GetSingleTweetById(long id)
+        {
+            var response = await Tweets.GetSingleTweetById(id);
+            return response;
+        }
+
+        [HttpGet]
+        [Route("Tweets/TweetsFromUser/{username}")]
+        public async Task<DefaultResponse<List<ITweetsBasicInformations>>> GetTweetsFromUserById(string username)
+        {
+            var response = await Tweets.GetTweetsFromUserByUsername(username);
+            return response;
+        }
+
+        [HttpPost]
+        [Route("CreateTweet")]
+        // POST: CreateTweet
+        public async Task<DefaultResponse<ITweetsData>> CreateTweet(string status)
+        {
+            var response = await Tweets.CreateTweet(status);
+            return response;
+        }
+    }
+}
