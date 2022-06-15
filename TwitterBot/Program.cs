e9f8e7078bded20785fe4c2a86005ae390bdac63
Vrossi28 <vrossi28@outlook.com>
2022-06-15 16:18:23 +0400

    Console list following don't follow back

--- TwitterBot/Program.cs
+++ TwitterBot/Program.cs
@@ -14,11 +14,10 @@ namespace TwitterBot
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Follower Finder");
-            Console.WriteLine("User: ");
+            Console.WriteLine("Follow Tracker");
+            Console.Write("User: ");
             string username = Console.ReadLine();
             var user = await Users.GetBaseDataByUsername(username);
-            //var userId = user.Result.Data.data.id;
 
             if(!user.Message.Equals("OK"))
             {
@@ -35,8 +34,12 @@ namespace TwitterBot
 
                 List<UserBaseData> followingDontFollowBack = followingResponse.Data.data.Where(i => followersResponse.Data.data.Select(x => x.id).Contains(i.id) == false).ToList();
 
-                Console.WriteLine($"Followers dont followed: {followersDontFollowed.Count()}");
+                //Console.WriteLine($"Followers dont followed: {followersDontFollowed.Count()}");
                 Console.WriteLine($"Following dont follow you back: {followingDontFollowBack.Count()}");
+                foreach (var following in followingDontFollowBack)
+                {
+                    Console.WriteLine(following.username);
+                }
 
             }
                 
