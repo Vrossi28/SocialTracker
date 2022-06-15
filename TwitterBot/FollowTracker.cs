b86510221e8146022f43b43d45e0ee444719ddb1
Vrossi28 <vrossi28@outlook.com>
2022-06-15 16:45:09 +0400

    Class FollowTracker and method improvements

---
+++ TwitterBot/FollowTracker.cs
@@ -0,0 +1,59 @@
+﻿using ExternalIntegration.Twitter.Models;
+using ExternalIntegration.Twitter.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterBot
+{
+    public static class FollowTracker
+    {
+        public static async Task FollowingDontFollowBack(string username)
+        {
+            var followersResponse = await Users.GetFollowersByUsername(username);
+            if(followersResponse.Data.data == null)
+            {
+                Console.WriteLine($"Error {followersResponse.Status} | {followersResponse.Message}");
+            }
+            var followingResponse = await Users.GetFollowingByUsername(username);
+
+            if (followingResponse.Data != null && followersResponse.Data != null)
+            {
+                List<UserBaseData> followingDontFollowBack = followingResponse.Data.data.Where(i => followersResponse.Data.data.Select(x => x.id).Contains(i.id) == false).ToList();
+
+                Console.WriteLine($"Following don't follow you back: {followingDontFollowBack.Count()}");
+                foreach (var following in followingDontFollowBack)
+                {
+                    Console.WriteLine(following.username);
+                }
+
+            }
+
+            Console.WriteLine($"{followersResponse.Message}");
+            return;
+        }
+
+        public static async Task FollowersDontFollowBack(string username)
+        {
+            var followersResponse = await Users.GetFollowersByUsername(username);
+            var followingResponse = await Users.GetFollowingByUsername(username);
+
+            if (followingResponse.Data != null && followersResponse.Data != null)
+            {
+                List<UserBaseData> followersDontFollowed = followersResponse.Data.data.Where(i => followingResponse.Data.data.Select(x => x.id).Contains(i.id) == false).ToList();
+
+                Console.WriteLine($"Followers you don't follow back: {followersDontFollowed.Count()}");
+                foreach (var following in followersDontFollowed)
+                {
+                    Console.WriteLine(following.username);
+                }
+
+            }
+
+            Console.WriteLine($"{followersResponse.Message}");
+            return;
+        }
+    }
+}
--- TwitterBot/Program.cs
+++ TwitterBot/Program.cs
@@ -16,36 +16,11 @@ namespace TwitterBot
         {
             Console.WriteLine("Follow Tracker");
             Console.Write("User: ");
+
             string username = Console.ReadLine();
-            var user = await Users.GetBaseDataByUsername(username);
 
-            if(!user.Message.Equals("OK"))
-            {
-                Console.WriteLine($"User: '{username}' was not found");
-                return;
-            }
-
-            var followersResponse = await Users.GetFollowersByUsername(username);
-            var followingResponse = await Users.GetFollowingByUsername(username);
-
-            if (followingResponse.Data != null && followersResponse.Data != null)
-            {
-                List<UserBaseData> followersDontFollowed = followersResponse.Data.data.Where(i => followingResponse.Data.data.Select(x => x.id).Contains(i.id) == false).ToList();
-
-                List<UserBaseData> followingDontFollowBack = followingResponse.Data.data.Where(i => followersResponse.Data.data.Select(x => x.id).Contains(i.id) == false).ToList();
-
-                //Console.WriteLine($"Followers dont followed: {followersDontFollowed.Count()}");
-                Console.WriteLine($"Following dont follow you back: {followingDontFollowBack.Count()}");
-                foreach (var following in followingDontFollowBack)
-                {
-                    Console.WriteLine(following.username);
-                }
-
-            }
-                
-            Console.WriteLine($"{followersResponse.Message}");
-            return;
-
+            //FollowTracker.FollowersDontFollowBack(username);
+            await FollowTracker.FollowingDontFollowBack(username);
         }
     }
 }
