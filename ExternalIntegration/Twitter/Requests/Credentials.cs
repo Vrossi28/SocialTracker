18fbad5ea55e67f430bcd7ecf9a442f3461b8610
Vinicius Rossi <57651321+Vrossi28@users.noreply.github.com>
2022-06-21 15:58:10 +0400

    Update Credentials.cs

--- ExternalIntegration/Twitter/Requests/Credentials.cs
+++ ExternalIntegration/Twitter/Requests/Credentials.cs
@@ -2,14 +2,14 @@
 {
     public static class Credentials
     {
-        public static string Bearer { get; private set; } = "AAAAAAAAAAAAAAAAAAAAAJ31dQEAAAAAMgt8fFoRcU%2BCbVmiGG%2B9COHp2dM%3DPhHsURQK726lJx0XAvGzdQlTB8Z2bQcD4BpRybrwfMhggtsthE";
-        public static string ConsumerKey { get; private set; } = "OH1Axraqn10aCcaqy0LOPL4tK";
-        public static string ConsumerKeySecret { get; private set; } = "y1s3rR72Rsabrncb5yhxycyBjJdW7xOT3gRtlALHonARQ66F3q";
-        public static string AccessToken { get; private set; } = "1347955168495755264-TObGEJ4SVUeqj0smGiIW2ZrZfnVEmV";
-        public static string AccessTokenSecret { get; private set; } = "P9ein9rJXIvt4T3105i3G6FsYoTeMOxLSHLfIfwwJcu4M";
-        public static string ClientId { get; private set; } = "cG9BWWN1VTE3b3VZdElxbGMzMW46MTpjaQ";
-        public static string ClientIdSecret { get; private set; } = "qnI7fdM0Y2J_JUAcKRiatGNarjKtKT2GbGdivc7sItghmYYxgf";
-        public static string IdUser { get; private set; } = "1347955168495755264";
+        public static string Bearer { get; private set; } = "BEARER_TOKEN";
+        public static string ConsumerKey { get; private set; } = "CONSUMER_KEY";
+        public static string ConsumerKeySecret { get; private set; } = "CONSUMER_SECRET";
+        public static string AccessToken { get; private set; } = "ACCESS_TOKEN";
+        public static string AccessTokenSecret { get; private set; } = "ACCESS_TOKEN_SECRET";
+        public static string ClientId { get; private set; } = "CLIENT_ID";
+        public static string ClientIdSecret { get; private set; } = "CLIENT_ID_SECRET";
+        public static string IdUser { get; private set; } = "ID_USER";
 
     }
 }
