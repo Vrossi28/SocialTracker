c176cd4f87f04ed997c1e6489e0e9b32642880db
Vrossi28 <vrossi28@outlook.com>
2022-06-23 17:15:24 +0400

    Merge SocialTracker

--- ExternalIntegration/Twitter/Requests/Credentials.cs
+++ ExternalIntegration/Twitter/Requests/Credentials.cs
@@ -2,16 +2,6 @@
 {
     public static class Credentials
     {
-<<<<<<< HEAD
-        public static string Bearer { get; private set; } = "AAAAAAAAAAAAAAAAAAAAAJ31dQEAAAAAp1QEy%2FjvtX6%2BTAJUGupSjf7FN90%3DryKPMbrUqB83V6vOOzplbxFzdUli1WjvTgG6n4oVU6kWVtNcEb";
-        public static string ConsumerKey { get; private set; } = "ydvo1Y7Z6ZOO6Zc5TW307eSiK";
-        public static string ConsumerKeySecret { get; private set; } = "eUt6W0NX5IFRzB1eqofrsUUyYxSlznqEFgODxT3kaTAZWLxkPd";
-        public static string AccessToken { get; private set; } = "1347955168495755264-F7EFJzRDASMrrrgYcTOdXksVlcZ2Le";
-        public static string AccessTokenSecret { get; private set; } = "EXUq8iCX9tCijKP7FbSwT6EkNAg9R7UQYX8x8eHaPUCVK";
-        public static string ClientId { get; private set; } = "cG9BWWN1VTE3b3VZdElxbGMzMW46MTpjaQ";
-        public static string ClientIdSecret { get; private set; } = "f5dNNwnvDCe-ZNJrlvcm5mTF1djhgnnvDyWveZmmy0ZjobneFh";
-        public static string IdUser { get; private set; } = "1347955168495755264";
-=======
         public static string Bearer { get; private set; } = "BEARER_TOKEN";
         public static string ConsumerKey { get; private set; } = "CONSUMER_KEY";
         public static string ConsumerKeySecret { get; private set; } = "CONSUMER_SECRET";
@@ -20,7 +10,6 @@
         public static string ClientId { get; private set; } = "CLIENT_ID";
         public static string ClientIdSecret { get; private set; } = "CLIENT_ID_SECRET";
         public static string IdUser { get; private set; } = "ID_USER";
->>>>>>> 18fbad5ea55e67f430bcd7ecf9a442f3461b8610
 
     }
 }
